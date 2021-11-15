6f56c62d7f04c7d292da47303e9c369b0def5ab0
aparanjisingh03 <84464895+aparanjisingh03@users.noreply.github.com>
2021-11-15 12:45:09 +0530

    Add files via upload

---
+++ binary,freindfun( )(6.2).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class complex
+{
+	private:
+		int real,imag;
+	public:
+		void getdata()
+		{
+			cout<<"enter real and imagnary parts:"<<endl;
+			cin>>real>>imag;
+		}
+	friend void operator +(complex &c1,complex &c2);
+};
+void operator +(complex &c1,complex &c2)
+{
+	cout<<c1.real+c2.real<<"+"<<c1.imag+c2.imag<<"i";
+}
+int main()
+{
+	complex c1,c2;
+	c1.getdata();
+	c2.getdata();
+	c1+c2;
+	return 0;
+}
